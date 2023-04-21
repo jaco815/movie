2627c6b2b9dab1a6886fecf804ab1c449d457742
DESKTOP-K5B4R34\admin <djaco@naver.com>
2023-04-21 15:30:57 +0900

    3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
 import React from 'react';
+import PropTypes from 'prop-types'; 
 
-function Food({name,pic}){
+function Food({name,picture,rating}){
   return(
     <div>
-      <h3> I Like {name} </h3>     
-      <img  src={pic}/>
+      <h3> I Like {name} </h3>    
+      <h4>{rating}/5.0</h4> 
+      <img  src={picture} alt={name}/>
     </div>
   ) 
 }
@@ -15,21 +17,24 @@ const foodLike = [
     id: 0,
     name : '01',
     image : 'http://www.aeriskitchen.com/wp-content/uploads/2023/01/SikBbang_Pizza_01-.jpg',
+    rating: 4.5,
   },
   {
     id: 1, 
     name: '02',
     image: 'http://www.aeriskitchen.com/wp-content/uploads/2022/11/Potato_Jeon_01-.jpg',
+    rating: 5,
   },
   {
     id: 2,
     name: '03',
     image: 'http://www.aeriskitchen.com/wp-content/uploads/2022/09/Salad_Sandwich_17-.jpg',
+    rating: 3.5,
   }
 ] ; 
 
 function renderFood(i){
-  return <Food key={i.id} name={i.name} pic={i.image} />
+  return <Food key={i.id} name={i.name} picture={i.image} rating={i.rating} />
 }
 
 function App() {
@@ -40,6 +45,12 @@ function App() {
       {foodLike.map(renderFood)}
     </div>
   );
+}
+
+Food.prototype={
+  name:PropTypes.string.isRequired,
+  picture:PropTypes.string.isRequired,
+  rating:PropTypes.number,
 }
 
 export default App;
