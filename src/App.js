10f9f4e5e98e16189a52aa6ca778c4ebe723e35b
DESKTOP-K5B4R34\admin <djaco@naver.com>
2023-04-21 15:54:34 +0900

    4

--- src/App.js
+++ src/App.js
@@ -1,56 +1,30 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
 import React from 'react';
-import PropTypes from 'prop-types'; 
 
-function Food({name,picture,rating}){
-  return(
-    <div>
-      <h3> I Like {name} </h3>    
-      <h4>{rating}/5.0</h4> 
-      <img  src={picture} alt={name}/>
-    </div>
-  ) 
-}
+class App extends React.Component{
 
-const foodLike = [
-  {
-    id: 0,
-    name : '01',
-    image : 'http://www.aeriskitchen.com/wp-content/uploads/2023/01/SikBbang_Pizza_01-.jpg',
-    rating: 4.5,
-  },
-  {
-    id: 1, 
-    name: '02',
-    image: 'http://www.aeriskitchen.com/wp-content/uploads/2022/11/Potato_Jeon_01-.jpg',
-    rating: 5,
-  },
-  {
-    id: 2,
-    name: '03',
-    image: 'http://www.aeriskitchen.com/wp-content/uploads/2022/09/Salad_Sandwich_17-.jpg',
-    rating: 3.5,
+  state = {
+    count: 3 ,
+  };
+
+  add = () =>{
+    console.log('add');
+    this.setState({count: this.state.count + 1})
+  };
+
+  minus = () =>{
+    console.log('minus');
+    this.setState({count:this.state.count-1})
+  };
+  render(){
+    return(
+      <div>
+        <h3> This number is {this.state.count} </h3>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    ) 
   }
-] ; 
-
-function renderFood(i){
-  return <Food key={i.id} name={i.name} picture={i.image} rating={i.rating} />
-}
-
-function App() {
-  console.log(foodLike.map(renderFood))
-  return (
-    <div className="App">
-      {/* { foodLike.map((n,i) =>(<Food name={i.name} pic={i.image}/>))} */}
-      {foodLike.map(renderFood)}
-    </div>
-  );
-}
-
-Food.prototype={
-  name:PropTypes.string.isRequired,
-  picture:PropTypes.string.isRequired,
-  rating:PropTypes.number,
 }
 
 export default App;
