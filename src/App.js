7004042aec7ee5ce4d6337b7b5aa8d8d06c49083
DESKTOP-K5B4R34\admin <djaco@naver.com>
2023-04-24 14:21:22 +0900

    4.24

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component{
 
   state = {
     isLoading : true ,
+    movies:[],
   }
 
+  getMovies = async () => {
+    const {data} = await axios.get('https://yts.mx/api/v2/list_movies.json');
+    // Object.assign(this.state.movies,data.data.movies);
+    this.setState({movies:data.data.movies});
+    this.setState({isLoading:false});
+    console.log(this.state.movies);  
+  };
+
   componentDidMount(){
-    setTimeout(()=>{
-      this.setState({isLoading:false});
-    },3000);
+    // setTimeout(()=>{
+    //   this.setState({isLoading:false});
+    // },5000);
+    this.getMovies();
+    // axios.get('https://yts.mx/api/v2/list_movies.json')
+    // .then(res => { 
+    //   this.setState({movies:res.data.data.movies , isLoading:false});
+    // })
+
   }
 
   render(){
 
     const {isLoading} = this.state ; 
+    const {movies} = this.state;
 
     return(
       <div>
-        { isLoading? 'Loading...': 'ready...'}
+        { isLoading? 'Loading...': 'we ar ready'}
+        <br/>
+        {movies.length}
+        <br/>
+        
+        {movies.map(movie => {
+          return(
+            <div key={movie.id}>
+              <h1>{movie.title}</h1>
+              <h2>{movie.rating}</h2>
+              <img src={movie.medium_cover_image} alt="movie-poster"/>
+            </div>
+          )
+        })}
       </div>
     ) 
   }
